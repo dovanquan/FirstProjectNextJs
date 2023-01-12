7a9d2e0980ba22f05bd759816e02e9be242f9028
quandv90 <quandv.adnetplus@gmail.com>
2023-01-12 16:12:29 +0700

    first commit

---
+++ pages/user.js
@@ -0,0 +1,8 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import React from "react";
+
+export default function User() {
+  return (
+    <div>'hello nextjs'</div>
+  )
+}
